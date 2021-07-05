bfd5cce5090debb0a5abbd91bbc568e25d5ab2a6
Daniel Bonnauer <bonnauer@predic8.de>
2021-07-05 15:12:28 +0200

    added necesary models

---
+++ src/main/java/com/predic8/scooter/model/Verfuegbarkeit.java
@@ -0,0 +1,20 @@
+package com.predic8.scooter.model;
+
+public class Verfuegbarkeit {
+    boolean verfuegbar;
+
+    public Verfuegbarkeit() {
+    }
+
+    public Verfuegbarkeit(boolean verfuegbar) {
+        this.verfuegbar = verfuegbar;
+    }
+
+    public boolean isVerfuegbar() {
+        return verfuegbar;
+    }
+
+    public void setVerfuegbar(boolean verfuegbar) {
+        this.verfuegbar = verfuegbar;
+    }
+}
